09b184bb761be458ca2683f69e98ac13e5ca3cb8
Rowan Cannaday <cannadayr@gmail.com>
2021-01-16 17:48:07 -0500

    add initial runtime bytecode and metadata

--- src/ebqn.erl
+++ src/ebqn.erl
@@ -1,3 +1,10 @@
 -module(ebqn).
 
--export([]).
+-export([runtime/1]).
+
+runtime(b) ->
+    <<15,1,25>>;
+runtime(o) ->
+    {0,1,2,32,3,8,infinity,neg_infinity,-1};
+runtime(s) ->
+    [{0,1,0,0}].
