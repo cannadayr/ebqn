cdb9a68aadcc73f18e02f56c585fcadb3b400012
Rowan Cannaday <cannadayr@gmail.com>
2021-03-14 19:10:26 -0400

    to_list/1 and concat/2 for ebqn_array

--- src/ebqn_array.erl
+++ src/ebqn_array.erl
@@ -4,7 +4,7 @@
 % https://erlang.org/doc/man/maps.html
 % https://en.wikipedia.org/wiki/Hash_array_mapped_trie
 
--export([new/1,get/2,set/3]).
+-export([new/1,get/2,set/3,to_list/1,concat/2]).
 
 % functions that need to be implemented (or have shims for)
 new(N) when is_integer(N),N > 0 ->
@@ -20,3 +20,10 @@ get(N,M) ->
 
 set(N,V,M) ->
     M#{N=>V}.
+
+to_list(M) ->
+    maps:fold(fun(_,V,A) -> [V]++A end,[],M).
+
+concat(X,W) ->
+    S = maps:size(X),
+    maps:fold(fun(K,V,A) -> A#{K+S=>V} end,X,W).
