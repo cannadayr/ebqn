bb17a32a413f35d73c767c220650dba1d7c32db6
Rowan Cannaday <cannadayr@gmail.com>
2021-03-14 19:22:32 -0400

    list -> ebqn_array

--- src/ebqn_array.erl
+++ src/ebqn_array.erl
@@ -4,9 +4,8 @@
 % https://erlang.org/doc/man/maps.html
 % https://en.wikipedia.org/wiki/Hash_array_mapped_trie
 
--export([new/1,get/2,set/3,to_list/1,concat/2]).
+-export([new/1,get/2,set/3,to_list/1,concat/2,from_list/1]).
 
-% functions that need to be implemented (or have shims for)
 new(N) when is_integer(N),N > 0 ->
     new(0,N,#{}).
 new(N,L,A) when N =:= L ->
@@ -23,6 +22,10 @@ set(N,V,M) ->
 
 to_list(M) ->
     maps:fold(fun(_,V,A) -> [V]++A end,[],M).
+
+from_list(L) ->
+    {_,R} = lists:foldl(fun(V,{N,A}) -> {N+1,A#{N=>V}} end,{0,#{}},L),
+    R.
 
 concat(X,W) ->
     S = maps:size(X),
