d2d68585caa0610a04a71f3fa1023ce58571816d
Rowan Cannaday <cannadayr@gmail.com>
2021-03-30 20:21:20 -0400

    simplify heap. makes gc easier.

--- src/ebqn_heap.erl
+++ src/ebqn_heap.erl
@@ -1,12 +1,17 @@
 -module(ebqn_heap).
 
--export([alloc/3,get/3,set/4]).
+-export([alloc/3,get/3,set/4,slots/2]).
 
 alloc(E,Slots,Heap) ->
-    maps:fold(fun(K,V,A) -> A#{{E,K}=>V} end,Heap,Slots).
+    maps:put(E,Slots,Heap).
 
 get(E,N,Heap) ->
-     maps:get({E,N},Heap).
+    Slots = maps:get(E,Heap),
+    ebqn_array:get(N,Slots).
 
 set(E,N,V,Heap) ->
-    Heap#{{E,N}=>V}.
+    #{E:=Slots} = Heap,
+    Heap#{E=>ebqn_array:set(N,V,Slots)}.
+
+slots(E,Heap) ->
+    maps:get(E,Heap).
