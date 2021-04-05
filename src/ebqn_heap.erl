c9e43b7911e1caea9e38d969b1f4708ff0f8013f
Rowan Cannaday <cannadayr@gmail.com>
2021-04-05 17:23:51 -0400

    rm rest of rust stuff

--- src/ebqn_heap.erl
+++ src/ebqn_heap.erl
@@ -1,11 +1,6 @@
 -module(ebqn_heap).
 
--include("crates.hrl").
-
 -export([alloc/3,get/3,set/4,slots/2]).
--export([static_atom/0,native_add/2,tuple_add/1,init_st/0,alloc/1]).
-
--on_load(init/0).
 
 alloc(E,Slots,Heap) ->
     maps:put(E,Slots,Heap).
@@ -20,21 +15,3 @@ set(E,N,V,Heap) ->
 
 slots(E,Heap) ->
     maps:get(E,Heap).
-
-init() ->
-    ?load_nif_from_crate(ebqn, ?crate_ebqn_heap, 0).
-
-static_atom() ->
-    exit(nif_library_not_loaded).
-
-native_add(_X, _Y) ->
-    exit(nif_library_not_loaded).
-
-tuple_add(_X) ->
-    exit(nif_library_not_loaded).
-
-init_st() ->
-    exit(nif_library_not_loaded).
-
-alloc(_E) ->
-    exit(nif_library_not_loaded).
