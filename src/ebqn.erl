465d1c11c7588250eed83928ef99ef1c0a0b44d8
Rowan Cannaday <cannadayr@gmail.com>
2021-04-09 13:37:21 -0400

    simplify mod1/mod2 call fns

--- src/ebqn.erl
+++ src/ebqn.erl
@@ -39,15 +39,13 @@ call(St0,F,X,W) when is_record(F,fn) ->
 call(St0,R,X,W) when is_record(R,d1) ->
     M = R#d1.m,
     F = R#d1.f,
-    Fn = M#r1.f,
-    D = Fn(F),
+    D = M(F),
     call(St0,D,X,W);
 call(St0,R,X,W) when is_record(R,d2) ->
     M = R#d2.m,
     F = R#d2.f,
     G = R#d2.g,
-    Fn = M#r2.f,
-    D = Fn(F,G),
+    D = M(F,G),
     call(St0,D,X,W);
 call(St0,F,X,W) when is_record(F,bi) ->
     0 = F#bi.t,
@@ -80,12 +78,12 @@ call1(St0,M,F) when is_record(M,bi) ->
     true = (1 =:= M#bi.t),
     call_block(St0,M,ebqn_array:from_list([M,F]));
 call1(St0,M,F) when is_record(M,r1) ->
-    {St0,#d1{m=M,f=F}}.
+    {St0,#d1{m=M#r1.f,f=F}}.
 call2(St0,M,F,G) when is_record(M,bi) ->
     true = (2 =:= M#bi.t),
     call_block(St0,M,ebqn_array:from_list([M,F,G]));
 call2(St0,M,F,G) when is_record(M,r2) ->
-    {St0,#d2{m=M,f=F,g=G}}.
+    {St0,#d2{m=M#r2.f,f=F,g=G}}.
 
 ge(I,E,An) when I =:= 0 ->
     E;
