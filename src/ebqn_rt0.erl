9b94d1d35d74902d49d1bf994420b02882c87b39
Rowan Cannaday <cannadayr@gmail.com>
2021-05-05 18:58:09 -0400

    stub rt0 module w/ prev written non-working fold

---
+++ src/ebqn_rt0.erl
@@ -0,0 +1,13 @@
+-module(ebqn_rt0).
+
+-include("schema.hrl").
+
+-export([fold/4]).
+
+fold(St0,F,X,undefined) ->
+    Size = maps:size(X#a.r),
+    R0 = ebqn_array:get(Size-1,X#a.r),
+    R1 = ebqn_array:get(Size-2,X#a.r),
+    RTail = ebqn_array:drop(2,X#a.r),
+    Init = ebqn:call(St0,F,R0,R1),
+    ebqn_array:foldr(fun(I,E,{StAccm,A}) -> ebqn:call(StAccm,F,A,E) end,Init,RTail).
