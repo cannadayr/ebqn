4b4a223be8d9b720910dfb72e4e341cec384bcca
Rowan Cannaday <cannadayr@gmail.com>
2021-05-15 23:42:01 -0400

    add flip and init fns to ebqn_mut

--- src/ebqn_mut.erl
+++ src/ebqn_mut.erl
@@ -1,17 +1,32 @@
 -module(ebqn_mut).
 
 % mutable byte arrays using atomics
--export([new/1,set/3]).
+-export([new/1,get/2,put/3,flip/2,init/3]).
 
 % create a new mutable byte array
 new(Size) ->
     atomics:new(Size,[{signed,false}]).
 
-% set contiguous indices in byte array
-set(Ref,Offset,Limit) ->
-    set(Ref,1,Offset,1+Limit).
-set(Ref,Id,Offset,Limit) when Id =/= Limit ->
-    ok = atomics:put(Ref,Id,Id+Offset),
-    set(Ref,1+Id,Offset,Limit);
-set(Ref,Id,Offset,Limit) when Id =:= Limit ->
+% 0-based indexing wrappers
+get(Ref,Id) ->
+    atomics:get(Ref,1+Id).
+put(Ref,Id,X) ->
+    atomics:put(Ref,1+Id,X).
+% flip entire byte array to max unsigned int
+% this is to distinguish between index 0 and an unset index
+flip(Ref,Size) ->
+    flip(Ref,0,Size).
+flip(Ref,Id,Size) when Id =/= Size ->
+    Max = (1 bsl 64) - 1,
+    ok = atomics:put(Ref,1+Id,Max),
+    flip(Ref,1+Id,Size);
+flip(_Ref,Id,Size) when Id =:= Size ->
+    ok.
+% sets the heap indices of the initial arguments
+init(Ref,Offset,Cnt) ->
+    init(Ref,0,Offset,Cnt).
+init(Ref,Id,Offset,Cnt) when Cnt =/= 0 ->
+    ok = ebqn_mut:put(Ref,Id,Id+Offset),
+    init(Ref,1+Id,Offset,Cnt-1);
+init(_Ref,_Id,_Size,Cnt) when Cnt =:= 0 ->
     ok.
